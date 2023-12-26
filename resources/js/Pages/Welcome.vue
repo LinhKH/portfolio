ba19aefc9ef7c3783218fc8a160260c8b0f6c023
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-26 19:06:15 +0700

    create promote and about section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,6 +2,8 @@
 import { Head, Link } from '@inertiajs/vue3';
 import FrontendLayout from '@/Layouts/Frontend.vue';
 import Hero from '../Components/Frontend/Hero.vue';
+import Promote from "@/Components/Frontend/Promote.vue";
+import About from "@/Components/Frontend/About.vue";
 
 </script>
 
@@ -11,7 +13,9 @@ import Hero from '../Components/Frontend/Hero.vue';
         <!-- Hero primary -->
         <Hero />
         <!-- Promote tail-100 -->
+        <Promote />
         <!-- About -->
+        <About />
         <!-- Skills -->
         <!-- Hero primary -->
 
