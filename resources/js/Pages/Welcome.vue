142c0c488f5ff594e771dcc47dd170c48048dbf5
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-26 20:53:49 +0700

    create skills section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,7 +4,12 @@ import FrontendLayout from '@/Layouts/Frontend.vue';
 import Hero from '../Components/Frontend/Hero.vue';
 import Promote from "@/Components/Frontend/Promote.vue";
 import About from "@/Components/Frontend/About.vue";
+import Skills from "@/Components/Frontend/Skills.vue";
 
+defineProps({
+    skills: Object,
+    projects: Object
+});
 </script>
 
 <template>
@@ -17,6 +22,7 @@ import About from "@/Components/Frontend/About.vue";
         <!-- About -->
         <About />
         <!-- Skills -->
+        <Skills :skills="skills" />
         <!-- Hero primary -->
 
     </FrontendLayout>
