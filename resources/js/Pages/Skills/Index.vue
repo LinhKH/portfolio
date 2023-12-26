f984629545921084ea1a7ad468d3d8af590116c1
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2023-12-26 10:44:50 +0700

    add link for create page

--- resources/js/Pages/Skills/Index.vue
+++ resources/js/Pages/Skills/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 
 
 </script>
@@ -14,7 +14,11 @@ import { Head } from '@inertiajs/vue3';
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">Table</div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-end m-2 p-2">
+                    <Link href="/skills/create" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">New Skill</Link>
+                </div>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
