9e299a4382d7962ce0e370ed11d4faf5e9ad03d7
Kieu Hoa LInh <l_kieu@vision-net.co.jp>
2024-05-10 12:50:10 +0700

    modify layout

--- resources/js/Components/Frontend/Skills.vue
+++ resources/js/Components/Frontend/Skills.vue
@@ -9,13 +9,9 @@ defineProps({
             <div class="flex flex-col items-center text-center">
                 <h2 class="section-title">Skills</h2>
             </div>
-            <div class="grid grid-cols-8 md:grid-flow-col">
-                <div
-                    class="flex items-center justify-center"
-                    v-for="skill in skills.data"
-                    :key="skill.id"
-                >
-                    <img :src="skill.image" :alt="skill.name" class="lg:h-20" />
+            <div class="flex flex-wrap gap-6 mt-4 justify-center">
+                <div class="" v-for="skill in skills.data" :key="skill.id">
+                    <img :src="skill.image" :alt="skill.name" width="170" class="lg:h-20" />
                 </div>
             </div>
         </div>
